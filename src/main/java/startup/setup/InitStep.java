ffbda4fd782f1792c9d5f0140cdb999714708e4e
Gregor <gregorgott.software@yahoo.com>
2025-03-26 10:25:16 +0100

    doc: add java doc to InitStep.java

--- src/main/java/startup/setup/InitStep.java
+++ src/main/java/startup/setup/InitStep.java
@@ -1,5 +1,17 @@
 package startup.setup;
 
+/**
+ * Interface for application initialization steps.
+ * An initialization step contains the following information:
+ *
+ * <ul>
+ *     <li>name for debug</li>
+ *     <li>description for app admin</li>
+ *     <li>task to execute</li>
+ * </ul>
+ *
+ * @author Gregor Gottschewski
+ */
 public interface InitStep {
     String name();
     String description();
