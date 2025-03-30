bb97c25c9a62d1a747c2a41ff915d1597c81ce76
Gregor <gregorgott.software@yahoo.com>
2025-03-30 21:28:32 +0200

    fix startup error and add preflight checks (db)

--- src/main/java/startup/ApplicationStartup.java
+++ src/main/java/startup/ApplicationStartup.java
@@ -1,11 +1,16 @@
 package startup;
 
 import api.Status;
-import jakarta.annotation.PostConstruct;
-import jakarta.enterprise.context.ApplicationScoped;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 import jakarta.inject.Inject;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
 import startup.setup.InitStep;
 import startup.setup.SetupException;
+import org.bson.Document;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,8 +18,8 @@ import java.util.List;
 
 import static io.ApplicationDirectories.APPLICATION_HOME;
 
-@ApplicationScoped
-public class ApplicationStartup {
+@WebListener(value = "Application startup")
+public class ApplicationStartup implements ServletContextListener {
 
     @Inject
     private Status status;
@@ -45,14 +50,57 @@ public class ApplicationStartup {
             }
     );
 
-    @PostConstruct
+    private static final List<InitStep> preFlightChecks = List.of(
+            new InitStep() {
+                @Inject
+                private Status status;
+
+                @Override
+                public String name() {
+                    return "MongoDB connection check";
+                }
+
+                @Override
+                public String description() {
+                    return "Checks if MongoDB is running on localhost:27017";
+                }
+
+                @Override
+                public void execute() {
+                    try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
+                        MongoDatabase database = mongoClient.getDatabase("admin");
+                        database.runCommand(new Document("ping", 1));
+                        System.out.println("MongoDB is running");
+                    } catch (Exception e) {
+                        status.addException(new SetupException(this, e.getMessage()));
+                    }
+                }
+            }
+    );
+
     public void init() {
-        for (InitStep initStep : initSteps) {
-            initStep.execute();
+        if (!Files.exists(APPLICATION_HOME.getPath())) {
+            for (InitStep initStep : initSteps) {
+                initStep.execute();
+            }
+        }
+
+        for (InitStep preFlightCheck : preFlightChecks) {
+            preFlightCheck.execute();
         }
 
         if (status.getExceptionCount() == 0) {
             status.setRunning(true);
         }
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        init();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        // Cleanup
     }
 }
