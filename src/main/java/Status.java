4d6034f75fa074fd795d6861c692cbf056d404c3
Gregor <gregorgott.software@yahoo.com>
2025-03-31 07:35:15 +0200

    add general status class

---
+++ src/main/java/Status.java
@@ -0,0 +1,26 @@
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.ApplicationScoped;
+
+/**
+ * This application scoped bean stores the status of the application.
+ * With the available flag, the general status of the application can be checked.
+ *
+ * @author Gregor Gottschewski
+ */
+@ApplicationScoped
+public class Status {
+    private boolean available;
+
+    @PostConstruct
+    public void init() {
+        available = false;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+}
