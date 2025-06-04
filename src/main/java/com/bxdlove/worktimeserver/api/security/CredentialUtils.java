243a167123dca8b6cce2b07a2a2f6c05e2ef08b8
Gregor <gregorgott.software@yahoo.com>
2025-06-04 13:59:25 +0200

    add admin login data validator

--- src/main/java/com/bxdlove/worktimeserver/api/security/CredentialUtils.java
+++ src/main/java/com/bxdlove/worktimeserver/api/security/CredentialUtils.java
@@ -26,4 +26,20 @@ public class CredentialUtils {
                     .first() != null;
         }
     }
+
+    /**
+     * Validates if specified user is an admin.
+     *
+     * @param username username of the admin user
+     * @param password password of the admin user
+     * @return {@code true} if the specified user is admin, {@code false} otherwise
+     */
+    public static boolean validateAdmin(String username, String password) {
+        try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
+            return mongoClient.getDatabase("worktime_server")
+                    .getCollection("employee")
+                    .find(new Document("email", username).append("password", password).append("admin", true))
+                    .first() != null;
+        }
+    }
 }
